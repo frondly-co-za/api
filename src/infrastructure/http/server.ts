e557e7513f3a2254a3f4566307a143c56a1672b6
Veli Gebrev <vgebrev@gmail.com>
2026-03-29 16:56:31 +0200

    feat: Add Swagger to API

--- src/infrastructure/http/server.ts
+++ src/infrastructure/http/server.ts
@@ -1,9 +1,18 @@
 import Fastify from 'fastify';
+import swagger from '@fastify/swagger';
+import swaggerUi from '@fastify/swagger-ui';
 import plantsRoute from './routes/plants.js';
 import dbConnector from './plugins/db-connector.js';
 import services from './plugins/services.js';
 
 const fastify = Fastify({ logger: true });
+
+fastify.register(swagger, {
+    openapi: {
+        info: { title: 'Frondly API', version: '0.0.1' }
+    }
+});
+fastify.register(swaggerUi, { routePrefix: '/swagger' });
 
 fastify.register(dbConnector);
 fastify.register(services);
